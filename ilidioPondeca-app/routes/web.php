a32d2fed9c167f912164028e44aae22637475530
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2022-12-05 23:46:39 +0200

    Update web.php

--- ilidioPondeca-app/routes/web.php
+++ ilidioPondeca-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\webController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rota para enviar email
+Route::post('/enviar_email',[webController::class,'enviar_email'])->name('enviar_email');
